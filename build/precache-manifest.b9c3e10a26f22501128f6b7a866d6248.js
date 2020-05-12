94bb6f420cd81e0ec3c256464e264f77a92caba9
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-12 18:00:14 -0500

    heroku troubleshooting

---
+++ build/precache-manifest.b9c3e10a26f22501128f6b7a866d6248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54e9d789df1c8bae66cc4d631a11d20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0b21953163481e9c9e9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3dd3006bb245f4ff2adf",
+    "url": "/static/js/2.01bf21c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01bf21c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b21953163481e9c9e9",
+    "url": "/static/js/main.48568856.chunk.js"
+  },
+  {
+    "revision": "b8f443678b3e1f6ff9f0",
+    "url": "/static/js/runtime-main.d02c6e08.js"
+  }
+]);
